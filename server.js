7f75dd8c135d49fb6209a24412d8b434620f2aaa
daryldixon1994 <kmohamedanouar@gmail.com>
2023-12-19 15:16:05 +0100

    first

---
+++ server.js
@@ -0,0 +1,8 @@
+// globals
+// __dirname: the absolute path of your directory
+console.log(__dirname);
+console.log(__filename);
+let password = "1245fdsqffdds";
+let privateKey = "ffbfbdifziffinfdfnddfdfonzd";
+
+let hashedPassword = password + privateKey;
